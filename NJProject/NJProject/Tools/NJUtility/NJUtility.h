33a6b9ed618570f91709d74a5729dd8f569e5447
lirenqiang <958246321@qq.com>
2017-08-07 09:43:41 +0800

    添加Utility

---
+++ NJProject/NJProject/Tools/NJUtility/NJUtility.h
@@ -0,0 +1,13 @@
+//
+//  NJUtility.h
+//  NJProject
+//
+//  Created by lirenqiang on 2017/8/7.
+//  Copyright © 2017年 Ninja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NJUtility : NSObject
+
+@end
---
+++ NJProject/NJProject/Tools/NJUtility/NJUtility.m
@@ -0,0 +1,13 @@
+//
+//  NJUtility.m
+//  NJProject
+//
+//  Created by lirenqiang on 2017/8/7.
+//  Copyright © 2017年 Ninja. All rights reserved.
+//
+
+#import "NJUtility.h"
+
+@implementation NJUtility
+
+@end
